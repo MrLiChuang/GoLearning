db50fa9d12f022029e31b586bd4b1226f6893db9
MrLiChuang <1430606284@qq.com>
2021-03-15 23:04:39 +0800

    标题:helloWorld   提交人 :lostApril

---
+++ 01 helloWorld.go
@@ -0,0 +1,12 @@
+//导入主函数的包，main函数必须在 main package下
+package main
+
+//format包 标准输入输出格式包
+import "fmt"
+
+//main函数 程序住入口
+//有且只有一个main函数
+//func 关键字代表函数定义
+func main() {
+	fmt.Print("Hello World!")
+}
